e9a7377776815d34293478ab58ee8e7059b4327c
JunHwaPark <a28165415@gmail.com>
2020-09-23 11:00:46 +0900

    이것이 코딩테스트다 - Chapter 11 그리디 기출1

---
+++ Greedy/모험가 길드 - 한빛.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+result = 0
+count = 0
+
+for i in arr:
+    count += 1
+    if i <= count:
+        result += 1
+        count = 0
+print(result)
