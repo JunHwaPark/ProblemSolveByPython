9d748fd680618736def4ea2ce55e6b5752870155
JunHwaPark <a28165415@gmail.com>
2020-10-05 15:49:26 +0900

    BOJ - 18310 (수정)

--- Sort/안테나 - BOJ.py
+++ Sort/안테나 - BOJ.py
@@ -4,6 +4,4 @@ house.sort()
 if n % 2 == 1:
     print(house[len(house) // 2])
 else:
-    val1 = house[len(house) // 2]
-    val2 = house[len(house) // 2 - 1]
-    print(min(val1, val2))
+    print(house[len(house) // 2 - 1])
