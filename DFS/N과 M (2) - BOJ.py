5cf121a55527b3712f2dac2361381c194bb0eaf3
JunHwaPark <a28165415@gmail.com>
2020-10-23 20:49:53 +0900

    BOJ - 15650

---
+++ DFS/N과 M (2) - BOJ.py
@@ -0,0 +1,24 @@
+from itertools import combinations
+from copy import deepcopy
+
+n, m = map(int, input().split())
+# for comb in combinations(range(1, n + 1), m):
+#     for i in comb:
+#         print(i, end=' ')
+#     print()
+#
+
+
+def backtracking(comb, index, num):
+    if len(comb) == num:
+        for i in comb:
+            print(i, end=' ')
+        print()
+    elif len(comb) > n:
+        return
+
+    for i in range(index, n + 1):
+        backtracking(comb[:] + [i], i + 1, num)
+
+
+backtracking(list(), 1, m)
